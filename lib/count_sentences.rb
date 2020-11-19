691e26ae9aa20bdbb99203a85b933d2665588388
choim3 <choim3@spu.edu>
2020-11-18 22:33:26 -0800

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
+  #come-back
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if { |x| x.size < 1}.size
+    binding.pry
   end
 end
---
+++ lib/test.rb
@@ -0,0 +1,4 @@
+array = [a,b,c,d]
+new_array = [b,a,c,d]
+
+new_array.each {|value| puts value}
